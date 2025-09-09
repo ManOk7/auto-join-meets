ab3fad80038dd957106d96d2652562e72add1db7
Manak Kushwaha <87892110+ManOk7@users.noreply.github.com>
2025-09-09 20:01:22 +0530

    Update auto-join-meets.py

--- auto-join-meets.py
+++ auto-join-meets.py
@@ -3,38 +3,47 @@ import time
 import os
 import schedule
 
-def join(link):
+def join(link, duration):
+    print(f"Joining meeting: {link} for {duration} minutes")
+
     os.system(f'"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" {link}')
-    time.sleep(5)  
+    time.sleep(5)  # wait for Chrome to open
 
-    pyautogui.click(x=671, y=887) #camera
-    pyautogui.click(x=558, y=872) #mic
-
+    # Turn off cam & mic
+    pyautogui.click(x=671, y=887)  # camera
+    pyautogui.click(x=558, y=872)  # mic
     time.sleep(1)
-    pyautogui.click(x=1498, y=660) #join button
+
+    # Join now button
+    pyautogui.click(x=1498, y=660)
+    print("Joined successfully!")
+    
+    # meeting duration
+    time.sleep(duration * 60)
+
+    # Leave meeting 
+    pyautogui.click(x=1140, y=1010) 
 
 
-#input from user
-n = int(input("How many meeting do you want to schedule:"))
+# input from user
+n = int(input("How many meetings do you want to schedule: "))
 
 meetings = {}
 
 for i in range(n):
     t = input("Enter time for meeting (HH:MM, 24hr format): ")
-    link = input("Enter Google Meet link for meeting:")
-    meetings[t] = link
-    
+    link = input("Enter Google Meet link for meeting: ")
+    d = int(input("Enter duration (in minutes) for meeting: "))
+    meetings[t] = {"link": link, "duration": d}
 
-for t, link in meetings.items():
-    schedule.every().day.at(t).do(join, link)
-    print(f"Meeting scheduled at {t} : {link}")
+
+# Scheduling meetings
+for t, info in meetings.items():
+    schedule.every().day.at(t).do(join, info["link"], info["duration"])
+    print(f"Meeting scheduled at {t} : {info['link']} : ({info['duration']} min)")
 
 print("Program started")
 
 while True:
     schedule.run_pending()
     time.sleep(60)
-
-
-
-
